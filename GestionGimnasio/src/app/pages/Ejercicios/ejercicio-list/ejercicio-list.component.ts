b1deb7072ce3ae8967ab4cf5a25a33b4f3614050
Alem Solano <alem.solano@ucr.ac.cr>
2025-04-30 01:01:10 -0600

    Modificacion para mostrar nombre de categoria
    Se modificó en la card el id de la categoria y se muestra el nombre

--- GestionGimnasio/src/app/pages/Ejercicios/ejercicio-list/ejercicio-list.component.ts
+++ GestionGimnasio/src/app/pages/Ejercicios/ejercicio-list/ejercicio-list.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { EjercicioService } from '../../../services/ejercicio.service';
+import { CategoriaEjercicioService } from '../../../services/categoriaEjercicio.service';
 import { Ejercicio } from '../../../Domain/Ejercicio.interface';
+import { Categoria } from '../../../Domain/CategoriaEjercicio.interface';
 import { EjercicioAddComponent } from '../ejercicio-add/ejercicio-add.component';
 
 @Component({
@@ -15,14 +17,19 @@ import { EjercicioAddComponent } from '../ejercicio-add/ejercicio-add.component'
 export class EjercicioListComponent implements OnInit {
   ejercicios: Ejercicio[] = [];
   ejerciciosFiltrados: Ejercicio[] = [];
+  categorias: Categoria[] = []; // Lista de categorías cargadas
   searchTerm: string = '';
   isLoading = true;
   mostrarFormulario: boolean = false; // Controla si se muestra el formulario o la lista
 
-  constructor(private ejercicioService: EjercicioService) {}
+  constructor(
+    private ejercicioService: EjercicioService,
+    private categoriaEjercicioService: CategoriaEjercicioService
+  ) {}
 
   ngOnInit(): void {
     this.cargarEjercicios();
+    this.cargarCategorias(); // Cargar las categorías al iniciar
   }
 
   cargarEjercicios(): void {
@@ -37,6 +44,22 @@ export class EjercicioListComponent implements OnInit {
         this.isLoading = false;
       },
     });
+  }
+
+  cargarCategorias(): void {
+    this.categoriaEjercicioService.getAllCategorias().subscribe({
+      next: (categorias) => {
+        this.categorias = categorias;
+      },
+      error: (err) => {
+        console.error('Error al cargar las categorías:', err);
+      },
+    });
+  }
+
+  obtenerNombreCategoria(idCategoria: number): string {
+    const categoria = this.categorias.find((cat) => cat.idCategoria === idCategoria);
+    return categoria ? categoria.nombreCategoria : 'Sin categoría';
   }
 
   filtrarEjercicios(): void {
