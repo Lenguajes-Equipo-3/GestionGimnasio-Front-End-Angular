f2dc56ae64e51b442a05cdc5b933ccc9960d5104
povedaJ <66696920+povedaJ@users.noreply.github.com>
2025-04-30 05:45:17 -0600

    Reapply "CRUD medidas corporales"
    This reverts commit c34ccb6da674d68c5a15aef84583d8f248e3eea0.

--- GestionGimnasio/src/app/pages/medidaCorporal/medida-corporal-list/medida-corporal-list.component.ts
+++ GestionGimnasio/src/app/pages/medidaCorporal/medida-corporal-list/medida-corporal-list.component.ts
@@ -2,13 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { HttpClientModule } from '@angular/common/http'; // <-- AGREGA ESTO
+import { HttpClientModule } from '@angular/common/http';
 import { MedidasService, Medida,Categoria } from '../../../services/medidas-corporales.service'; 
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { EditarMedidaDialogComponent } from '../editar-medida-dialog/editar-medida-dialog.component';
+import { AgregarMedidaDialogComponent } from '../agregar-medida/agregar-medida.component';
+
 
 @Component({
   selector: 'app-medidas-corporales',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatIconModule, HttpClientModule], // <-- AGREGA HttpClientModule aquí
+  imports: [CommonModule,
+     MatCardModule, 
+     MatIconModule,
+     HttpClientModule,
+     MatDialogModule,
+     EditarMedidaDialogComponent,
+    AgregarMedidaDialogComponent ], 
   templateUrl: './medida-corporal-list.component.html',
   styleUrls: ['./medida-corporal-list.component.css']
 })
@@ -17,32 +27,26 @@ export class MedidasCorporalesComponent implements OnInit {
   medidasAgrupadas: { [key: string]: Medida[] } = {};  
   categorias: Categoria[] = [];
 
-  constructor(private medidasService: MedidasService) {}
-
+  constructor(private medidasService: MedidasService,  private dialog: MatDialog) {}
+ 
   ngOnInit() {
-    this.cargarMedidas();
-    this.cargarCategorias();
+    this.medidasService.obtenerCategorias().subscribe((categorias) => {
+      this.categorias = categorias;
+      console.log('Categorías cargadas:', this.categorias);
+  
+      this.medidasService.obtenerMedidas().subscribe((medidas) => {
+        this.medidas = medidas;
+        console.log('Medidas cargadas:', this.medidas);
+        this.agruparMedidasPorCategoria();
+      });
+    });
   }
 
-  cargarCategorias() {
-    this.medidasService.obtenerCategorias().subscribe((data) => {
-      this.categorias = data;
-      console.log('Categorías obtenidas:', this.categorias);
-      this.agruparMedidasPorCategoria();
-    });
-  }
-  cargarMedidas() {
-    this.medidasService.obtenerMedidas().subscribe((data) => {
-      this.medidas = data;
-      console.log('Medidas obtenidas:', this.medidas);
-    });
-  }
   agruparMedidasPorCategoria() {
-    this.medidasAgrupadas = {};  // Limpiar agrupación previa
-
+    this.medidasAgrupadas = {};  
     // Agrupar las medidas por el nombre de la categoría
     this.medidas.forEach(medida => {
-      const categoria = this.categorias.find(c => c.idCategoria === medida.idCategoria);  // Buscar la categoría por id
+      const categoria = this.categorias.find(c => c.idCategoria === medida.idCategoria); 
       if (categoria) {
         const categoriaNombre = categoria.nombreCategoria;
         if (!this.medidasAgrupadas[categoriaNombre]) {
@@ -52,11 +56,50 @@ export class MedidasCorporalesComponent implements OnInit {
       }
     });
   }
-  editarMedida(medida: Medida) {
-    console.log('Editar medida:', medida);
+
+  agregarMedida(categoria: any) {
+    const dialogRef = this.dialog.open(AgregarMedidaDialogComponent, {
+      width: '500px',
+      data: {
+        categorias: this.categorias,
+        idCategoriaSeleccionada: categoria.idCategoria 
+      },      
+    });
+  
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === 'added') {
+        this.ngOnInit();
+      }
+    });}
+  
+  editarMedida(medida: Medida): void {
+    console.log('Editar medida', medida);
+
+    const dialogRef = this.dialog.open(EditarMedidaDialogComponent, {
+      width: '500px',
+      data: {
+        medida,
+        categorias: this.categorias
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.ngOnInit();
+      }
+    });
+    
   }
 
-  eliminarMedida(medida: Medida) {
-    console.log('Eliminar medida:', medida);
+  
+  eliminarMedida(medida: Medida): void {
+    const confirm = window.confirm('¿Estás seguro de que deseas eliminar esta medida?');
+    if (confirm) {
+      this.medidasService.eliminarMedida(medida.idMedida).subscribe(() => {
+       this.ngOnInit();
+      });
+    }
   }
+  
+  
 }
