5b55d6a9383f2caed85e391b1ef5fc7d7821447c
Alem Solano <alem.solano@ucr.ac.cr>
2025-06-02 01:03:03 -0600

    Agregar resumen de rutina

--- GestionGimnasio/src/app/pages/Rutina/tabs/rutina-resumen-tab/rutina-resumen-tab.component.ts
+++ GestionGimnasio/src/app/pages/Rutina/tabs/rutina-resumen-tab/rutina-resumen-tab.component.ts
@@ -1,9 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../../../../services/cliente.service';
 import { Empleado } from '../../../../Domain/Empleado.interface';
 import { Subscription } from 'rxjs';
-import { ItemRutinaEjercicio } from '../../../../Domain/ItemRutinaEjercicio';
-import { ItemRutinaMedida } from '../../../../Domain/RutinaCompleta.interface';
 import { RutinaContextService } from '../../../../services/rutinaC.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -11,53 +9,38 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-rutina-resumen-tab',
-  imports: [CommonModule,
+  imports: [
+    CommonModule,
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule],
+    MatButtonModule
+  ],
   templateUrl: './rutina-resumen-tab.component.html',
   styleUrl: './rutina-resumen-tab.component.css'
 })
 export class RutinaResumenTabComponent implements OnInit {
-cliente: Cliente | null = null;
+  cliente: Cliente | null = null;
   empleado: Empleado | null = null;
-   itemRutiEjercicio: ItemRutinaEjercicio[]=[];
-    itemRutinaMedida :ItemRutinaMedida[]=[];
- private subscription!: Subscription;
-   objetivo = '';
- lesiones = '';
- enfermedades = '';
- fechaRenovacion!: Date;
- esVigente = true;
- medidadS=String;
- ejercicios=String;
- fechaCreacion: Date = new Date();
- @Output() datosGuardados = new EventEmitter<void>();
-  rutina:any;
+  rutina: any = null;
 
   constructor(private rutinaContextService: RutinaContextService) {}
 
   ngOnInit(): void {
-this.rutina = this.rutinaContextService.getRutinaActual(); 
-this.itemRutiEjercicio=this.rutina.getEjercicios();
-this.cliente=this.rutina.getCliente();
-this.itemRutinaMedida=this.rutina.getMedidas();
-this.empleado=this.rutina.getEmpleado;
-this.objetivo=this.rutina.getObjetivo();
-this.lesiones=this.rutina.getLesiones();
-this.enfermedades=this.rutina.getEnfermedades();
-this.fechaRenovacion=this.rutina.getFechaRenovacion();
-this.esVigente=this.rutina.getEsVigente();
-this.medidadS=this.rutina.medidas.toString;
+    this.rutina = this.rutinaContextService.getRutinaActual();
+    this.cliente = this.rutina.cliente ?? null;
+    this.empleado = this.rutina.empleado ?? null;
   }
 
-   guardarCambios(): void {}
-   guardarRutina(){}
+  getNombresEjercicios(): string {
+    if (!this.rutina?.ejercicios?.length) return 'Sin ejercicios';
+    return this.rutina.ejercicios
+      .map((e: any) => e.ejercicio?.nombreEjercicio)
+      .filter((n: string | undefined) => !!n)
+      .join(', ');
+  }
 }
-
